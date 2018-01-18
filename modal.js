90d7e2e67620ded2c4e061a195b47840d58ee022
Mike D <michael.mcdaniel210@gmail.com>
2018-01-17 21:48:31 -0600

    working modal js and index

---
+++ modal.js
@@ -0,0 +1,35 @@
+//disables clicking outside of modal to remove it:
+//this must remain outside of document.ready 
+$('#ageModal').modal({
+    backdrop: 'static',
+    keyboard: false
+});
+
+$(document).ready(function(){
+	// $("#ageModal").modal('show');
+	$("#ageSubmitButton").on("click", function(){
+		var userDate = $("#ageInput").val();
+		console.log("userDate" + userDate);				
+		var randomFormat = "YYYY/MM/DD";
+	    var convertedDate = moment(userDate, randomFormat);
+	    console.log("conv" + convertedDate);
+    	var compareDate = moment(convertedDate).diff(moment(), "years");
+ 		var diffDate = (compareDate * -1);
+ 		console.log("compare" + diffDate);
+ 		if (diffDate >= 21) {
+ 			$("#ageModal").modal("hide");
+ 		}else {
+ 			$("#modal-body").html("Enjoy a soda instead, kiddo!");
+ 			window.location.replace("http://berghoffbeer.com/sodas/");
+ 			
+ 		}
+
+	});
+
+
+
+
+
+
+
+});//closes document.ready function

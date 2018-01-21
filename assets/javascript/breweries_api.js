e1cd9c3110043ddc8900729a5f7a954aaaaa2951
gracepark01 <gracepark01@gmail.com>
2018-01-21 16:14:22 -0600

    Minor updates to layout in breweries.css and breweries_results.html; added maps link and other minor changes to breweries_api.js

--- assets/javascript/breweries_api.js
+++ assets/javascript/breweries_api.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-// vars to hold inputted city and state
+// Variables to hold inputted city and state
 var city;
 var state;
+var cityCap;
+var stateCap;
 
   // Function handles events when search button is clicked
   $("#location-submit").on("click", function(event) {
@@ -15,17 +17,28 @@ var state;
       // Stores inputted location
       city = $("#city-input").val().trim().toLowerCase();
       state = $("#state-input").val().trim().toLowerCase();
-      console.log(city + ", " + state);
+      cityCap = $("#city-input").val().trim().toUpperCase();
+      stateCap = $("#state-input").val().trim().toUpperCase();
 
       // Resets input fields
       $("#city-input").val("");
       $("#state-input").val("");
 
-    }
-    
-    // Runs search function
-    searchForBreweries();
+      // Results label
+      $("#breweries-results").html("Search results for " + cityCap + ", " + stateCap);
 
+      // Runs search function
+      searchForBreweries();
+
+    // For incomplete search inputs  
+    } else {
+      $("#breweries-results").empty();      
+      $("#breweries-row").html("<div class=col-md-12><p>Sorry, we couldn't find any breweries based on your search. Please try another search.</p></div>");
+
+      // Resets input fields
+      $("#city-input").val("");
+      $("#state-input").val("");
+    }
   });
 
   // Search function to pull BreweryDB API based on user inputted city and state
@@ -38,46 +51,38 @@ var state;
     }).done(function(response) {
       // console.log(response);
     
-        // var to hold results array
-        var results = response.data;
+      // var to hold results array
+      var results = response.data;
 
-        // Checks for valid search parameters
-        if (results.length != undefined) {
+      // Looks through each result from search and populates results page
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i].brewery.website);
+        var imageURL = results[i].brewery.images ?  results[i].brewery.images.squareMedium : "";
+        console.log(i + ' ' + imageURL);
+        var breweryImage = $("<img />", {
+          "src": imageURL
+        });
+        var breweryName = results[i].brewery.name;
+        console.log(breweryName);
+        var breweryWebsite = results[i].brewery.website;
+        console.log(breweryWebsite);
+        var breweryAddress = results[i].streetAddress;
+        console.log(breweryAddress);
+        var breweryId = "brewery" + i;
+        var breweryDiv = $("<div class='col-md-3 brewery' id='" + breweryId + "'>");
 
-          // Look through each result from search
-          for (var i = 0; i < results.length; i++) {
-            console.log(results[i].brewery.website);
-          
-            var imageURL = results[i].brewery.images ?  results[i].brewery.images.squareMedium : "";
-            console.log(i + ' ' + imageURL);
-            var breweryImage = $("<img />", {
-              "src": imageURL
-            });
-          
-            var breweryName = results[i].name;
-            console.log(breweryName);
-            var breweryWebsite = results[i].brewery.website;
-            console.log(breweryWebsite);
-            var breweryAddress = results[i].streetAddress;
-            console.log(breweryAddress);
+        // Only includes results with sufficient info
+        if (breweryName != undefined && breweryAddress != undefined && breweryWebsite != undefined) {
+          $("#breweries-row").append(breweryDiv);
+          $(breweryDiv).append(breweryImage);
+          $(breweryDiv).append("<p>" + breweryName + "</p>");
+          $(breweryDiv).append("<p><a href='https://www.google.com/maps/search/?api=1&query=" + breweryName + "+" + breweryAddress + "+" + city + "+" + state + "' target='_blank'>" + breweryAddress + "</a></p>");
+          $(breweryDiv).append("<p><a href='" + breweryWebsite + "' target='_blank'>" + breweryWebsite + "</a></p>");
 
-            var breweryId = "brewery" + i;
-            var breweryDiv = $("<div class='col-md-3 brewery' id='" + breweryId + "'>");
-
-            if (breweryImage.src != "unknown" && breweryName != undefined && breweryAddress != undefined && breweryWebsite != undefined) {
-
-              $("#breweries-row").append(breweryDiv);
-              $(breweryDiv).append(breweryImage);
-              $(breweryDiv).append("<p>" + breweryName + "</p>");
-              $(breweryDiv).append("<p>" + breweryAddress + "</p>");
-              $(breweryDiv).append("<p><a href='" + breweryWebsite + "' target='_blank'>" + breweryWebsite + "</a></p>");
-            } 
-          }
-        } else {
-          $("#breweries-row").append("<div=col-md-12><p>Sorry, we couldn't find a brewery based on your search. Please try another search.</p></div>")
-        }
+        } else if (breweryDiv = undefined) {
+            $("#breweries-row").append("<div class=col-md-12><p>Sorry, we couldn't find a brewery based on your search. Please try another search.</p></div>");
+        }  
+      } 
     }); 
   }   
 });
-
-// validate city name - no results if no results

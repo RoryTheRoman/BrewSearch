c651ef661b4bc9b993f08261d8956d1295543959
faunarr <fhouchin@gmail.com>
2018-01-21 17:59:14 -0600

    changed formatting and added if statement to only add if array is greater than 1

--- assets/javascript/beers_api.js
+++ assets/javascript/beers_api.js
@@ -1,53 +1,52 @@
 var beers = [];
 
-        function getBeerName() {
-          
-          var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search?q=" + beers + "&type=beer&key=214453339d6bbb99f8869c9524f3a6c1";
+function getBeerName() {
+    if (beers.length === 0) {
+      return;
+    }
 
-        $.ajax({
-          url: queryURL,
-          method: 'GET'
-          }).done(function(response) {
-          // console.log(response);
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search?q=" + beers + "&type=beer&key=214453339d6bbb99f8869c9524f3a6c1";
 
-          var results = response.data;
-          for (var i = 0; i < results.length; i++) {
-          
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var displayName = results[i].name;
+        var displayDescript = results[i].description;
+        var displayStyle = results[i].style.shortName;
+        var displayABV = results[i].abv;
 
-            var displayName = results[i].name;
-            var displayDescript = results[i].description;
-            var displayStyle = results[i].style.shortName;
-            var displayABV = results[i].abv;
+        if (displayName != undefined && displayDescript != undefined && displayStyle != undefined && displayABV != undefined) {
 
-            if (displayName != undefined && displayDescript != undefined && displayStyle != undefined && displayABV != undefined) {
-            
-
-             $("#beerTable > tbody").append("<tr><td>" + displayName + "</td><td>" + displayDescript + "</td><td>" +
-          displayStyle + "</td><td>" + displayABV + "</td></tr>")
-          };
-
-          $("reset").on("click", function() {
-            $("beerTable").empty();
-          });
-
+          $("#beerTable > tbody").append([
+            "<tr>",
+              "<td>", displayName, "</td>",
+              "<td>", displayDescript, "</td>",
+              "<td>", displayStyle, "</td>",
+              "<td>", displayABV, "</td>",
+            "</tr>"].join(""));
         };
-
-        });
-
-  };
-
-  $("#add-beer").on("click", function(event) {
-      event.preventDefault();
-
-      var beer = $("#beer-input").val().trim();
-     
-
-      beers.push(beer);
-
-      $("#beer-input").val("");
-
+      };
     });
+  };
   
- $(document).on("click", "#add-beer", getBeerName);
- $(document).on("click", "#reset", getBeerName);
+  $("#add-beer").on("click", function(event) {
 
+    event.preventDefault();
+
+    var beer = $("#beer-input").val().trim();
+    if (beer.length > 0) {
+      beers.push(beer);
+    }
+
+    $("#beer-input").val("");
+  });
+
+  $("#reset").on("click", function() {
+    beers = [];
+    $("#beerTable > tbody").empty();
+  });
+
+ $(document).on("click", "#add-beer", getBeerName);

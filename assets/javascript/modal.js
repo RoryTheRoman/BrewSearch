b39803893427ff4f55fbbeb5dffd222cf143a811
Mike D <michael.mcdaniel210@gmail.com>
2018-01-22 19:33:37 -0600

    abandon cookie, using local storage.  won't click or scroll upon revisit to home page

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -7,21 +7,15 @@ $(document).ready(function(){
     keyboard: false,
 	});
 	
-	// if (cookieValue !== 0)	{
-	// 		$("#ageModal").modal("hide")
-	// 	}else{
-	// 		$("#ageModal").modal("show");
+	
 
-	// 	} from https://github.com/js-cookie/js-cookie
-
-    // function readCookie(name) {
-    //   var nameEQ = name + "=";
-    //   var ca = document.cookie.split(";");
-
-    //   return null;
-    // } this is from the homework	
-
-	$("#ageModal").modal("show");
+    if (!localStorage.getItem('verified') || localStorage.getItem('verified') === "false") {
+    	console.log('yes')
+    	$("#ageModal").removeClass("hide");
+		$("#ageModal").modal("show");
+    	
+    }
+	// $("#ageModal").modal("show");
 	$("#ageSubmitButton").on("click", function(){
 		var userDate = $("#ageInput").val();
 		var randomFormat = "YYYY/MM/DD";
@@ -29,11 +23,11 @@ $(document).ready(function(){
     	var compareDate = moment(convertedDate).diff(moment(), "years");
  		var diffDate = (compareDate * -1);
  		if (diffDate >= 21) {
- 			var verified = Cookies.set("verified",1);
- 			console.log(verified);
  			$(".modal-open").removeClass("modal-open");
  			$("#ageModal, .modal-backdrop").modal("hide").remove();
+ 			localStorage.setItem('verified', true);
  		}else { 
+ 			localStorage.setItem('verified', false);
   			$(".modal-body").html("<div id='modalRedir'>Enjoy a soda-pop instead, kiddo!");
 			setTimeout(function(){ 			
  			window.location.replace("http://berghoffbeer.com/sodas/");		
@@ -41,19 +35,7 @@ $(document).ready(function(){
  		}
 		});
 
-			// if (verified == null){
-			// 	$("#ageModal").modal("show");
-			// }else {
-			// 	$("#ageModal").modal("hide");
-			// }
 
-    // if ($.cookie('verified') == null) {
-    //     $.cookie('verified', 'yes',);
-    //     $('#ageModal').modal("show");
-    //    } 
-
-// $("#modal").remove();
-// $('.modal-backdrop').remove(); this is the one i was trying to avoid having cookies altogether.
 
 
 
